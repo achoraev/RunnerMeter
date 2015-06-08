26490c994770f4264e4b8be4d1718f0b2133c272
Angel Raev <audulin@gmail.com>
2015-06-08 14:50:52 +0300

    set Map type to Hybrid and animate zoom

--- GoogleMap/src/main/java/com/newrunner/googlemap/MapsActivity.java
+++ GoogleMap/src/main/java/com/newrunner/googlemap/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.newrunner.googlemap;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -11,10 +15,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private Location location;
+    private LatLng myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
     }
@@ -25,27 +36,24 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
+//    private void centerMapOnMyLocation() {
+//        mMap.setMyLocationEnabled(true);
+//        location = mMap.getMyLocation();
+//        LatLng myLocation = new LatLng(42.678874,23.360368);
+//        if (location != null) {
+//            myLocation = new LatLng(location.getLatitude(),
+//                    location.getLongitude());
+//        }
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 2));
+//    }
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
@@ -53,13 +61,19 @@ public class MapsActivity extends FragmentActivity {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(42.678874, 23.360368)).title("Marker"));
+//        mMap.setMyLocationEnabled(true);
+//        location = mMap.getMyLocation();
+        myLocation = new LatLng(42.678874,23.360368);
+//        if (location != null) {
+//            myLocation = new LatLng(location.getLatitude(),
+//                    location.getLongitude());
+//        }
+//        Toast.makeText(this, String.format("lat: {} long: {}", location.getLatitude(), location.getLongitude()), Toast.LENGTH_SHORT).show();
+
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),
+//                location.getLongitude())).title("My Location"));
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 10));
     }
 }

3d9ab2bb8d48a66fbc112dc7846cc1e0744bcf38
Angel Raev <audulin@gmail.com>
2015-09-04 17:53:31 +0300

    make account view and fill data

--- GoogleMap/src/main/java/com/newrunner/googlemap/AccountActivity.java
+++ GoogleMap/src/main/java/com/newrunner/googlemap/AccountActivity.java
@@ -2,6 +2,7 @@ package com.newrunner.googlemap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
 import com.parse.ParseUser;
 
 import java.util.HashMap;
@@ -11,13 +12,38 @@ import java.util.HashMap;
  */
 public class AccountActivity extends Activity {
     private final HashMap accounts = new HashMap();
+    ProgressBar progress;
+    EditText name, userName, eMail, createdAt;
+    TextView isVerified;
+    Button editBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_layout);
 
-        // todo check if user contains
-        accounts.put(ParseUser.getCurrentUser().getUsername(), convertFromUserToAccount(ParseUser.getCurrentUser()));
+        // init views
+        progress = (ProgressBar) findViewById(R.id.progress_bar);
+
+        name = (EditText) findViewById(R.id.edit_name);
+        userName = (EditText) findViewById(R.id.edit_username);
+        eMail = (EditText) findViewById(R.id.edit_mail);
+        isVerified = (TextView) findViewById(R.id.edit_is_verified);
+        createdAt = (EditText) findViewById(R.id.edit_date);
+        editBtn = (Button) findViewById(R.id.edit_btn);
+
+        // todo save account hashmap to local datastore
+        if(!accounts.containsKey(ParseUser.getCurrentUser().getUsername())) {
+            accounts.put(ParseUser.getCurrentUser().getUsername(), convertFromUserToAccount(ParseUser.getCurrentUser()));
+        }
+
+        Account current = (Account) accounts.get(ParseUser.getCurrentUser().getUsername());
+
+        name.setHint(current.getName());
+        userName.setHint(current.getUserName());
+        eMail.setHint(current.getEmail());
+        isVerified.setText(current.getIsVerified().toString());
+        createdAt.setHint(current.getCreatedAt());
     }
 
     private Account convertFromUserToAccount(ParseUser currentUser) {

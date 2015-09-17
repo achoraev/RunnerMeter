1b45197a8efab9cb33061a186d7522108f9bbec5
Angel Raev <audulin@gmail.com>
2015-09-17 16:25:31 +0300

    files for recycle view adapter

---
+++ SportsMeter/src/main/java/com/newrunner/sportsmeter/activities/RecycleActivity.java
@@ -0,0 +1,36 @@
+package com.newrunner.sportsmeter.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.newrunner.sportsmeter.R;
+
+/**
+ * Created by angelr on 17-Sep-15.
+ */
+public class RecycleActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycle);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = new String[10];
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}

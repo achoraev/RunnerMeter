16e8cc559d29bddf517090b42422d624e522b0cf
Angel Raev <audulin@gmail.com>
2015-09-18 15:40:38 +0300

    change from list view to working recycle list view - latest technology

--- SportsMeter/src/main/java/com/runner/sportsmeter/activities/RecyclerActivity.java
+++ SportsMeter/src/main/java/com/runner/sportsmeter/activities/RecyclerActivity.java
@@ -1,18 +1,18 @@
-package com.newrunner.sportsmeter.activities;
+package com.runner.sportsmeter.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import com.newrunner.sportsmeter.R;
-import com.newrunner.sportsmeter.models.Session;
+import com.runner.sportsmeter.R;
+import com.runner.sportsmeter.models.Session;
 
 import java.util.ArrayList;
 
 /**
  * Created by angelr on 17-Sep-15.
  */
-public class RecycleActivity extends Activity {
+public class RecyclerActivity extends Activity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -33,7 +33,7 @@ public class RecycleActivity extends Activity {
 
         // specify an adapter (see also next example)
         ArrayList<Session> myDataset = new ArrayList<>();
-        mAdapter = new MyAdapter(myDataset);
+        mAdapter = new RecyclerAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ SportsMeter/src/main/java/com/runner/sportsmeter/activities/RecyclerAdapter.java
@@ -0,0 +1,76 @@
+package com.runner.sportsmeter.activities;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.runner.sportsmeter.R;
+import com.runner.sportsmeter.common.Calculations;
+import com.runner.sportsmeter.models.Session;
+
+import java.util.ArrayList;
+
+/**
+ * Created by angelr on 17-Sep-15.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private ArrayList<Session> dataList;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        private TextView distance, duration, maxSpeed, averageSpeed, createdAt, timePerKm, user;
+        public ViewHolder(View v) {
+            super(v);
+            distance = (TextView) v.findViewById(R.id.view_distance);
+            duration = (TextView) v.findViewById(R.id.view_duration);
+            maxSpeed = (TextView) v.findViewById(R.id.view_maxspeed);
+            averageSpeed = (TextView) v.findViewById(R.id.view_avrspeed);
+            timePerKm = (TextView) v.findViewById(R.id.view_timeperkm);
+            createdAt = (TextView) v.findViewById(R.id.view_created);
+            user = (TextView) v.findViewById(R.id.view_user);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public RecyclerAdapter(ArrayList<Session> myDataset) {
+        dataList = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.leaderboard_row, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.distance.setText(String.valueOf(dataList.get(position).getDistance()) + " m");
+        holder.duration.setText(String.valueOf(Calculations.roundToTwoDigitsAfterDecimalPoint(dataList.get(position).getDuration() / 1000 / 60) + " min"));
+        holder.maxSpeed.setText(String.valueOf(dataList.get(position).getMaxSpeed()) + " kmph");
+        holder.averageSpeed.setText(String.valueOf(dataList.get(position).getAverageSpeed()) + " kmph");
+        holder.timePerKm.setText(String.valueOf(dataList.get(position).getTimePerKilometer()) + " min/km");
+        holder.createdAt.setText(String.valueOf(dataList.get(position).getCreatedAt()));
+        holder.user.setText(String.valueOf(dataList.get(position).getUserName()));
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}

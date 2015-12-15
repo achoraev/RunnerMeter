92cd5fd6cac3187dfb2b8321f9e734a34d5a27f2
Angel Raev <audulin@gmail.com>
2015-12-15 17:09:30 +0200

    add on about legal notice for google map

--- SportsMeter/src/main/java/com/runner/sportsmeter/activities/AboutActivity.java
+++ SportsMeter/src/main/java/com/runner/sportsmeter/activities/AboutActivity.java
@@ -6,6 +6,8 @@ import android.widget.TextView;
 import com.runner.sportsmeter.BuildConfig;
 import com.runner.sportsmeter.R;
 
+import static com.google.android.gms.common.GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo;
+
 /**
  * Created by angelr on 09-Oct-15.
  */
@@ -14,7 +16,7 @@ public class AboutActivity extends Activity {
     private final String ADDRESS = "46, str. Liaskovets, \n 1510, Sofia, Bulgaria";
     private final String EMAIL = "runner.meter@gmail.com";
     private final String FACEBOOK_PAGE = "https://www.facebook.com/sportmeter/";
-    TextView buildVersion, companyAddress, companyEmail, companyFacebookPage;
+    private TextView buildVersion, companyAddress, companyEmail, companyFacebookPage, googleLegalInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,16 @@ public class AboutActivity extends Activity {
         // todo google legal notices
         // this must be used for Services attribution text as part of a "Legal Notices" section in your application.
         // Including legal notices as an independent menu item, or as part of an "About" menu item, is recommended.
-//        GoogleApiAvailability.getOpenSourceSoftwareLicenseInfo(AboutActivity.this);
+        String legal = getOpenSourceSoftwareLicenseInfo(AboutActivity.this);
         buildVersion = (TextView) findViewById(R.id.build_version);
+        googleLegalInfo = (TextView) findViewById(R.id.google_legal);
         companyAddress = (TextView) findViewById(R.id.company_address);
         companyEmail = (TextView) findViewById(R.id.company_email);
         companyFacebookPage = (TextView) findViewById(R.id.company_facebook_page);
 
         buildVersion.setText(getString(R.string.app_name) + " v. " + BuildConfig.VERSION_NAME);
         companyAddress.setText(ADDRESS);
+        googleLegalInfo.setText(legal);
         companyEmail.setText(EMAIL);
         companyFacebookPage.setText(FACEBOOK_PAGE);
     }

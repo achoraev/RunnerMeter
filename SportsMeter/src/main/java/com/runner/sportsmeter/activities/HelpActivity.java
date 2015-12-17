3d1378b8902f4f38a063a253458291ed127c325b
Angel Raev <audulin@gmail.com>
2015-12-17 17:16:13 +0200

    refactor help fragments and images

--- SportsMeter/src/main/java/com/runner/sportsmeter/activities/HelpActivity.java
+++ SportsMeter/src/main/java/com/runner/sportsmeter/activities/HelpActivity.java
@@ -8,13 +8,12 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import com.runner.sportsmeter.R;
 import com.runner.sportsmeter.common.ViewPagerAdapter;
+import com.runner.sportsmeter.fragments.HelpFragmentFour;
 import com.runner.sportsmeter.fragments.HelpFragmentOne;
 import com.runner.sportsmeter.fragments.HelpFragmentThree;
 import com.runner.sportsmeter.fragments.HelpFragmentTwo;
@@ -24,10 +23,9 @@ import com.runner.sportsmeter.fragments.HelpFragmentTwo;
  */
 public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    DrawerLayout drawerLayout;
-    CollapsingToolbarLayout collapsingToolbarLayout;
-    FloatingActionButton fab;
-    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private FloatingActionButton fab;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
@@ -36,17 +34,10 @@ public class HelpActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.l_help_layout);
 
-//        setupNavigationView();
-//        setupToolbar();
 //        setupCollapsingToolbarLayout();
         initializeViewPager();
         setupTablayout();
         setupFab();
-
-//        toolbar = (Toolbar) findViewById(R.id.toolbar2);
-//        setSupportActionBar(toolbar);
-
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void initializeViewPager() {
@@ -67,22 +58,18 @@ public class HelpActivity extends AppCompatActivity implements View.OnClickListe
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new HelpFragmentOne(), "Start page");
-        adapter.addFragment(new HelpFragmentTwo(), "Main page");
-        adapter.addFragment(new HelpFragmentThree(), "THREE");
+        adapter.addFragment(new HelpFragmentOne(), getString(R.string.tab_how_to_login));
+        adapter.addFragment(new HelpFragmentTwo(), getString(R.string.tab_how_start_page));
+        adapter.addFragment(new HelpFragmentThree(), getString(R.string.tab_main_page));
+        adapter.addFragment(new HelpFragmentFour(), getString(R.string.tab_save_page));
         viewPager.setAdapter(adapter);
-    }
-
-    private void setupNavigationView() {
-
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     private void setupCollapsingToolbarLayout() {
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         if (collapsingToolbarLayout != null) {
-            collapsingToolbarLayout.setTitle(toolbar.getTitle());
+//            collapsingToolbarLayout.setTitle(toolbar.getTitle());
             //collapsingToolbarLayout.setCollapsedTitleTextColor(0xED1C24);
             //collapsingToolbarLayout.setExpandedTitleColor(0xED1C24);
         }
@@ -100,33 +87,19 @@ public class HelpActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void setupToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar2);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-        }
-
-        // Show menu icon
-        final ActionBar ab = getSupportActionBar();
-        if (ab != null) {
-            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-            ab.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
     @Override
     public void onClick(View view) {
 
         if (view.getId() == R.id.fab) {
             Snackbar
-                    .make(findViewById(R.id.coordinatorLayout), "Skip How to start", Snackbar.LENGTH_LONG)
-                    .setAction("Skip", new View.OnClickListener() {
+                    .make(findViewById(R.id.coordinatorLayout), R.string.skip_how_start, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.skip, new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             finish();
                         }
                     })
-                    .show(); // Don’t forget to show!
+                    .show();
         }
     }
 }
